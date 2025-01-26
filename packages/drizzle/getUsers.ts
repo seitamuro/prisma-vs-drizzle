9ced1047f675f953306d41b3aee46b35b169def6
seitamuro <seitamuro0210@gmail.com>
2025-01-26 21:45:10 +0900

    明示的にコネクションを管理

--- packages/drizzle/getUsers.ts
+++ packages/drizzle/getUsers.ts
@@ -1,11 +1,18 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { usersTable } from "./src/db/schema";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const db = drizzle(pool);
 
 async function main() {
-  const users = await db.select().from(usersTable);
+  const users = await db
+    .select()
+    .from(usersTable)
+    .finally(() => pool.end());
   console.log(users);
 }
 
